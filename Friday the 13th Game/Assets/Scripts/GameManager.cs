a91337fcf3001856b53b5115f4704290fc79d683
Seth <sethcram@gmail.com>
2022-07-06 21:31:04 -0600

    game manager singleton w/ consolidated MainMenuButton()

--- Friday the 13th Game/Assets/Scripts/GameManager.cs
+++ Friday the 13th Game/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// game manager for keeping track of game data
@@ -15,6 +16,69 @@ public class GameManager : MonoBehaviourPun
 
     [HideInInspector]
     public int deadCounselors { private set; get; } = 0;
+
+    #region Singleton
+
+    private static GameManager instance = null;
+
+    /*
+     * Summary: Singleton definition of GameManager instance. 
+     *          //Persistent across scenes and used by other classes.
+     *          Created when needed.
+     * 
+     */
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                //find instance in scene
+                instance = FindObjectOfType<GameManager>();
+
+                //if no instance in scene
+                if (instance == null)
+                {
+                    //create new manager in scene:
+                    GameObject mnger = new GameObject();
+                    mnger.name = "GameManager";
+                    instance = mnger.AddComponent<GameManager>();
+
+                    Debug.LogWarning("new game manager created");
+
+                    //make sure not destroyed w/ change scenes
+                    //DontDestroyOnLoad(mnger);
+                }
+            }
+            return instance;
+        }
+    }
+
+    #endregion Singleton
+
+    /*
+     * Summary: Makes sure there's only one instance of Singleton GameManager in scenes.
+     *          Only checks if game manager script already attached to obj in scene.
+     * 
+     */
+    void Awake()
+    {
+        //instance not here
+        if (instance == null)
+        {
+            //set instance to this script (triggers above Instance method to create/destroy)
+            instance = this;
+            //DontDestroyOnLoad(this.gameObject);
+        }
+        //instance here
+        else
+        {
+            //destroy it
+            Destroy(gameObject);
+        }
+    }
+    
+    #region RPC's
 
     /// <summary>
     /// RPC to change start vote count.
@@ -35,6 +99,32 @@ public class GameManager : MonoBehaviourPun
     {
         deadCounselors = newDeadCounselorsCount;
 
+    }
+
+    #endregion RPC's
+
+    //called by main menu button:
+    public void MainMenuButton()
+    {
+        //disconnect client and load main menu:
+        StartCoroutine(DisconnectAndLoad());
+    }
+
+    //disconnect client and load main menu:
+    private IEnumerator DisconnectAndLoad()
+    {
+        //disconnect client from server:
+        PhotonNetwork.Disconnect();
+
+        //while still connected to network:
+        while (PhotonNetwork.IsConnected)
+        {
+            //dont yet load scene:
+            yield return null;
+        }
+
+        //load main menu if no longer connected:
+        SceneManager.LoadScene(0);
     }
 
 }
