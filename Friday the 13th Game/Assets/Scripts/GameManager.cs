72481e2f9916148328359245085a2ec40f1cacd4
Seth <sethcram@gmail.com>
2022-07-22 09:07:38 -0600

    state machine + wait till vars filled + same str compare, incr dead counselors change

--- Friday the 13th Game/Assets/Scripts/GameManager.cs
+++ Friday the 13th Game/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,13 +12,54 @@ using UnityEngine.SceneManagement;
 /// </summary>
 public class GameManager : MonoBehaviourPun 
 {
+    #region Vars
+
     [HideInInspector]
     public int startVotes { private set; get; } = 0; //needa incr/decr over RPC
 
     [HideInInspector]
     public int deadCounselors { private set; get; } = 0;
 
+    [HideInInspector]
     public int playersGameReady { private set; get; } = 0;
+
+    [HideInInspector]
+    public GameObject jasonPlayer;
+    [HideInInspector]
+    public GameObject ourPlayer;
+    [HideInInspector]
+    public PlayerManager playerManager;
+
+    private bool jasonLeft = false;
+
+    private string currSceneName;
+    private bool notGameScene;
+    private bool gameLobbyScene;
+
+    //state mach:
+    public enum State { MENU, INIT, PLAY, LOADLEVEL, GAMEOVER, PAUSE, SPECTATE};
+    private State _state;
+    private State unpauseState;
+    private bool _isSwitchingState;
+
+    //scene tracking vars
+    public enum CurrentScene { LOBBY, GAME_LOBBY, GAME };
+    public CurrentScene currentScene { private set; get; }
+
+    //to keep track of what panel active: (unneeded?)
+    private GameObject currPanel;
+    public GameObject panelMenu;
+    public GameObject panelPlay;
+    public GameObject panelLevelCompleted;
+    public GameObject panelGameOver;
+    public GameObject panelPause;
+    public GameObject panelHelp;
+
+    //music vars
+    private bool musicShouldPlay = true;
+    private AudioSource audioSrc;
+
+    #endregion Vars
 
     #region Singleton
 
@@ -60,6 +102,8 @@ public class GameManager : MonoBehaviourPun
 
     #endregion Singleton
 
+    #region Startup
+
     /*
      * Summary: Makes sure there's only one instance of Singleton GameManager in scenes.
      *          Only checks if game manager script already attached to obj in scene.
@@ -80,6 +124,168 @@ public class GameManager : MonoBehaviourPun
             //destroy it
             Destroy(gameObject);
         }
+
+        //store scene details
+        currSceneName = SceneManager.GetActiveScene().name;
+
+        //determine current scene
+        if (SameString_IgnoreCase(currSceneName, "Lobby"))
+        {
+            currentScene = CurrentScene.LOBBY;
+        }
+        else if (SameString_IgnoreCase(currSceneName, "Game Lobby"))
+        {
+            currentScene = CurrentScene.GAME_LOBBY;
+        }
+        else if (SameString_IgnoreCase(currSceneName, "Game"))
+        {
+            currentScene = CurrentScene.GAME;
+        }
+
+        //default state to menus
+        _state = State.MENU;
+    }
+
+    private void Start()
+    {
+        
+        notGameScene = currSceneName != "Game";
+        gameLobbyScene = currSceneName == "Game Lobby"; 
+
+        //if not starting in the lobby
+        if ( currentScene != CurrentScene.LOBBY )
+        {
+            //wait till our player instance filled to fit vars in
+            StartCoroutine(WaitTillPlayerInstanceFilled());
+        }
+
+        
+    }
+
+    /// <summary>
+    /// Waits asynchly till player instance filled to fill fields
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WaitTillPlayerInstanceFilled()
+    {
+        //while player inst not filled
+        while (ourPlayer == null)
+        {
+            Debug.Log("Wait 1 frame bc our player inst not filled.");
+
+            //wait a frame
+            yield return null;
+        }
+
+        //if our player is counselor
+        if (ourPlayer.tag == "Player")
+        {
+            //cache jason player
+            jasonPlayer = GameObject.FindGameObjectWithTag("Enemy");
+        }
+        //if our player is jason
+        else
+        {
+            //store our player as jason player
+            jasonPlayer = ourPlayer;
+        }
+
+        //player instance filled so cache player manager
+        playerManager = ourPlayer.GetComponent<PlayerManager>();
+
+        //startCheckingIfJasonLeft = true;
+    }
+
+    /// <summary>
+    /// return whether the passed in strings are the same ignoring case
+    /// </summary>
+    /// <param name="str1"></param>
+    /// <param name="str2"></param>
+    /// <returns></returns>
+    public bool SameString_IgnoreCase( string str1, string str2)
+    {
+        return string.Compare(str1, str2, StringComparison.OrdinalIgnoreCase) == 0;
+    }
+
+    #endregion Startup
+
+    private void Update()
+    {
+        /*
+        //if not game scene or jason already left or local play
+        if( notGameScene ||  jasonLeft || !PhotonNetwork.IsConnected)
+        {
+            //don't check if jason left
+            return;
+        }
+
+        //if(currentScene)
+
+        //if jason player empty
+        if(jasonPlayer == null)
+        {
+            //find jason player
+            jasonPlayer = GameObject.FindGameObjectWithTag("Enemy");
+
+            //if jason player actually left
+            if (jasonPlayer == null)
+            {
+                //make sure player manager filled
+                playerManager = ourPlayer.GetComponent<PlayerManager>();
+
+                //tell remaining players they won
+                playerManager.Win(isGameOver: true);
+                
+                //don't repeat telling players they won
+                jasonLeft = true;
+            }
+            //jason player didn't actually leave
+            else
+            {
+                playerManager = ourPlayer.GetComponent<PlayerManager>();
+            }
+        }
+        */
+
+        //stop playing music if it should stop:
+        if (musicShouldPlay == false)
+        {
+            //audioSrc.Stop();
+            audioSrc.Pause();
+        }
+
+        //run state code every frame
+        switch (_state) 
+        {
+            case State.INIT:
+                //spawn players and items
+                /*
+                if( playersSpawned )
+                {
+                    SwitchState(State.MENU);
+                }
+                */
+
+                break;
+            case State.MENU:
+
+                break;
+            case State.PLAY:
+                //alow player control
+                break;
+            case State.SPECTATE:
+                //allow player control
+                break;
+            case State.GAMEOVER:
+                //revoke player control
+                break;
+            case State.LOADLEVEL:
+                //revoke player control
+                break;
+            case State.PAUSE:
+                //revoke player control
+                break;
+        }
     }
 
     #region RPC's
@@ -95,13 +301,13 @@ public class GameManager : MonoBehaviourPun
     }
 
     /// <summary>
-    /// RPC to change # of dead counselors
+    /// RPC to incr # of dead counselors
     /// </summary>
     /// <param name="newDeadCounselorsCount"></param>
     [PunRPC]
-    public void RPC_ChangeCounselorsDead(int newDeadCounselorsCount)
+    public void RPC_IncrCounselorsDead()
     {
-        deadCounselors = newDeadCounselorsCount;
+        deadCounselors++;
 
     }
 
@@ -117,6 +323,104 @@ public class GameManager : MonoBehaviourPun
     }
 
     #endregion RPC's
+
+    #region State Methods
+
+    //to change state to passed in state:
+    public void SwitchState(State newState, float delay = 0) //aka next state logic process?
+    {
+        //print("Switch state to " + newState);
+
+        //waits zero seconds unless 'delay' arg overwritten:
+        StartCoroutine(SwitchDelay(newState, delay));
+    }
+
+    /// <summary>
+    /// Switch scenes with the given delay by ending the curr state and beginning the next
+    /// </summary>
+    /// <param name="newState"></param>
+    /// <param name="delay"></param>
+    /// <returns></returns>
+    private IEnumerator SwitchDelay(State newState, float delay) //changes _state to specified state and passes to end then begin after changing
+    {
+        _isSwitchingState = true;
+
+        yield return new WaitForSeconds(delay);
+
+        EndState();
+
+        _state = newState; //bc just changed states below
+
+        BeginState(newState);
+
+        _isSwitchingState = false;
+
+    }
+
+    //does operations based on current state
+    private void BeginState(State newState) //aka output logic process
+    {
+        //print("Current state: " + newState);
+
+        switch (newState)
+        {
+            case State.MENU:
+                
+                break;
+            case State.INIT: //for resetting score and such
+                
+                SwitchState(State.LOADLEVEL);
+                break;
+            case State.PLAY:
+                break;
+            case State.LOADLEVEL:
+
+                break;
+            case State.GAMEOVER:
+                //panelGameOver.SetActive(true);
+                break;
+            case State.PAUSE:
+                //panelPause.SetActive(true);
+                //pauseApp(); //apply pause settings
+                break;
+        }
+    }
+
+    private void EndState()
+    {
+        //print("State ending: " + _state);
+
+        switch (_state)
+        {
+            case State.MENU:
+                //panelMenu.SetActive(false);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                //musicShouldPlay = false;
+                //panelPlay.SetActive(false);
+                //panelGameOver.SetActive(false);
+                break;
+            case State.PAUSE:
+                //should resume playing paused audio src :
+                //audioSrc.Play();
+                //musicShouldPlay = true;
+
+                //Cursor.visible = false;
+                //panelPause.SetActive(false);
+                //Time.timeScale = 1; //resume time
+                break;
+        }
+    }
+
+    #endregion State Methods
+
+    #region Load Main Menu
 
     //called by main menu button:
     public void MainMenuButton()
@@ -141,5 +445,5 @@ public class GameManager : MonoBehaviourPun
         //load main menu if no longer connected:
         SceneManager.LoadScene(0);
     }
-
+    #endregion Load Main Menu
 }
