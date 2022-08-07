a82954e1124aa169c54f7f47ad970dc543c31e36
sethhi2 <sethcram@gmail.com>
2022-08-06 20:14:53 -0600

    Editor + play test folders added and first play test started

---
+++ Friday the 13th Game/Assets/Scripts/Tests/PlayModeTests/TestDying.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+//using SethScripts.UI;
+
+namespace Tests
+{
+    public class TestDying: MonoBehaviour
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestDyingSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestDyingWithEnumeratorPasses()
+        {
+            //load main menu if no longer connected:
+            SceneManager.LoadScene(0);
+
+            //wait for main menu to load
+            yield return new WaitForSeconds(1);
+
+            //can't find other classes bc of assembly issues
+            //FindObjectOfType<MainMenu>()
+
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
