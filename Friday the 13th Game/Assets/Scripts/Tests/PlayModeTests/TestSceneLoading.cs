bd308a4ebd1853f27670b650bc37e3bf7b898baf
sethhi2 <sethcram@gmail.com>
2022-08-15 21:38:32 -0600

    Verify photon connection and scene after loading into lobby, run coroutine from within a coroutine for reusability

--- Friday the 13th Game/Assets/Scripts/Tests/PlayModeTests/TestSceneLoading.cs
+++ Friday the 13th Game/Assets/Scripts/Tests/PlayModeTests/TestSceneLoading.cs
@@ -4,31 +4,41 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
+using System;
 //using SethScripts.UI;
 
 namespace Tests
 {
     public class TestSceneLoading: MonoBehaviour
     {
-        // A Test behaves as an ordinary method
-        [Test]
-        public void TestDyingSimplePasses()
+
+        [UnityTest]
+        public IEnumerator TestCallingMainMenuToLobby()
         {
-            // Use the Assert class to test conditions
+            //wait till lobby loaded
+            yield return TestLoadingMainMenuToLobby();
+
+            print("After coroutine called.");
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
+        /// <summary>
+        /// Load from the main menu into the lobby using play button function.
+        /// By default: verifies we're in lobby scene + Photon Network connected.
+        /// </summary>
+        /// <returns></returns>
         [UnityTest]
         public IEnumerator TestLoadingMainMenuToLobby()
         {
             int framesWaited = 0;
 
-            //load main menu if no longer connected:
+            //lobbySceneLoadedAndItsMethodsExed = false;
+
+            //load main menu:
             SceneManager.LoadScene(GameManager.MAIN_MENU_SCENE_NAME);
 
             //wait for main menu to load
-            while(GameManager.Instance.currentScene != GameManager.CurrentScene.MAIN_MENU)
+            while (GameManager.Instance.currentScene != GameManager.CurrentScene.MAIN_MENU)
             {
                 yield return null;
 
@@ -41,7 +51,7 @@ namespace Tests
             MainMenu mainManu = FindObjectOfType<MainMenu>();
 
             //while can't find main menu
-            while(mainManu == null)
+            while (mainManu == null)
             {
                 yield return null;
 
@@ -56,6 +66,8 @@ namespace Tests
             //simulate user clicking play
             mainManu.PlayGame();
 
+            //loading scene also gone thru here
+
             //wait for lobby to load
             while (GameManager.Instance.currentScene != GameManager.CurrentScene.LOBBY)
             {
@@ -64,8 +76,45 @@ namespace Tests
             }
             Debug.Log($"{framesWaited} frames waited for lobby to load.");
 
-            // Use the Assert class to test conditions.
-            Assert.True(true, "Connected to Lobby.");
+            //verify we're in the lobby scene
+            VerifyCurrentScene(desiredSceneName: GameManager.LOBBY_SCENE_NAME);
+
+            //verify connnected to Photon network
+            VerifyNetworkConnected();
+        }
+
+        /// <summary>
+        /// Verify we're connected to the Photon Network.
+        /// </summary>
+        public void VerifyNetworkConnected()
+        {
+            //pass test if connected to photon network
+            Assert.True(PhotonNetwork.IsConnected, "Not connected to the Photon Network.");
+            //success msg
+            Debug.Log("Connected to the Photon Network.");
+        }
+    
+        /// <summary>
+        /// Verify the current and desired scene names are the same.
+        /// </summary>
+        /// <param name="desiredSceneName"></param>
+        public void VerifyCurrentScene(string desiredSceneName)
+        {
+            string currSceneName = SceneManager.GetActiveScene().name;
+
+            //pass test if current scene matches desired scene
+            Assert.True
+                (
+                    //return true if curr scene name is lobby scene name
+                    GameManager.Instance.SameString_IgnoreCase
+                    (
+                        currSceneName,
+                        desiredSceneName
+                    ),
+                    $"Current scene is {currSceneName} not {desiredSceneName}."
+                );
+            //success msg
+            Debug.Log($"Current scene is {desiredSceneName}.");
         }
     }
 }
